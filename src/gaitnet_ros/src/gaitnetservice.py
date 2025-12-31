d3e404fbb94a325abafc9844660625efd6140d69
owen-sullivan <opsullivan85@gmail.com>
2025-12-31 20:46:00 +0000

    added in gaitnet dependency

--- src/gaitnet_ros/src/gaitnetservice.py
+++ src/gaitnet_ros/src/gaitnetservice.py
@@ -1,26 +1,39 @@
-from pathlib import Path
+try:
+    import gaitnet
+except ImportError as e:
+    raise ImportError(
+        "gaitnet package not found. Please ensure that gaitnet is installed and accessible." \
+        "\nTry running 'pip install -e .' from `/extern/gaitnet/` (making sure the submodule is initialized)."
+    ) from e
 
+from pathlib import Path
 import torch
 import rospy
-
-import gaitnet_interface.msg as gn_msg
 from gaitnet_interface.srv import (
     GaitNetInterfaceRequest as Req,
     GaitNetInterfaceResponse as Res,
     GaitNetInterface,
 )
 
-
+from gaitnet.gaitnet.gaitnet import GaitnetActor
 
 class GaitNetService:
-    def __init__(self, device: str, model_path: Path) -> None:
-        pass
+    def __init__(self, device: torch.device, model_path: Path) -> None:
+        self.device = device
+        self.model_path = model_path
+        self.model: torch.nn.Module = self.load_model(model_path, device)
+        rospy.loginfo(
+            f"GaitNetService initialized with device {self.device} and model path {self.model_path}"
+        )
+
+    @staticmethod
+    def load_model(model_path: Path, device: torch.device) -> torch.nn.Module:
+        rospy.loginfo(f"Loading model from {model_path} on device {device}")
+        return torch.nn.Module()
 
     def handle_request(self, req: Req) -> Res:
+        return Res()
 
-        rospy.loginfo(req)
-        req.stepCSpace.FL.legCSpace
-        return Res()  # Placeholder response
 
 if __name__ == "__main__":
     rospy.init_node("gaitnet_service")
@@ -29,7 +42,11 @@ if __name__ == "__main__":
     if type(device_str) is not str:
         rospy.logerr("Device parameter must be a string")
         exit(1)
-    device = torch.device(device_str)
+    try:
+        device = torch.device(device_str)
+    except Exception as e:
+        rospy.logerr(f"Invalid device parameter: {e}")
+        exit(1)
 
     model_path_str = rospy.get_param("~model_path")
     if type(model_path_str) is not str:
@@ -39,7 +56,6 @@ if __name__ == "__main__":
     if not model_path.exists():
         rospy.logerr(f"Model path {model_path} does not exist")
         exit(1)
-
 
     gaitnet_service = GaitNetService(device=device, model_path=model_path)
     service = rospy.Service(
